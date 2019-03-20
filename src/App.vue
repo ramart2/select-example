4820c1df85816d0c1f12625666dc49b2524f8394
Rudy Sanchez <ramart2@asu.edu>
2019-03-20 11:27:44 -0700

    Adding the code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+	  <div>
+		  <div>
+			  <label class="typo__label" for="ajax">Async multiselect</label>
+			  <multiselect
+				  v-model="value"
+				  id="ajax"
+				  label="name"
+				  track-by="code"
+				  placeholder="Type to search"
+				  open-direction="bottom"
+				  :options="filterOptions"
+				  :searchable="true"
+				  :loading="isLoading" :internal-search="false"
+				  :close-on-select="true"
+				  :show-no-results="true"
+				  @search-change="find">
+				  <template slot="clear" slot-scope="props">
+					  <div class="multiselect__clear"></div>
+				  </template><span slot="noResult">Oops! No elements found. Consider changing the search query.</span>
+			  </multiselect>
+			  <pre class="language-json"><code>{{ value  }}</code></pre>
+		  </div>
+	  </div>
+
+	  <div v-if="showModal" class="modal">
+		  <div class="modal-background"></div>
+		  <div class="modal-content">
+			  you have updated the table
+			  <!-- Any other Bulma elements you want -->
+		  </div>
+		  <button @click="showModal = false" class="modal-close is-large" aria-label="close"></button>
+	  </div>
+
+	  <button @click="showModal = true">Click</button>
+
+    <!--<v-popover-->
+      <!--offset="16"-->
+    <!--&gt;-->
+      <!--&lt;!&ndash; This will be the popover target (for the events and position) &ndash;&gt;-->
+      <!--<button class="tooltip-target b3">Click me</button>-->
+
+      <!--&lt;!&ndash; This will be the content of the popover &ndash;&gt;-->
+      <!--<template slot="popover">-->
+        <!--<input class="tooltip-content" v-model="msg" placeholder="Tooltip content" />-->
+        <!--<p>-->
+          <!--{{ msg }}-->
+        <!--</p>-->
+
+        <!--&lt;!&ndash; You can put other components too &ndash;&gt;-->
+         <!--<img alt="Vue logo" src="./assets/logo.png">-->
+        <!--<HelloWorld msg="Welcome to Your Vue.js App" char="="/>-->
+        <!--&lt;!&ndash; <ExampleComponent char="=" /> &ndash;&gt;-->
+      <!--</template>-->
+    <!--</v-popover>-->
   </div>
 </template>
 
@@ -12,17 +65,180 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+	data() {
+        return {
+        	showModal: false,
+            value: null,
+	        isLoading: false,
+            options: [
+		        { name: 'Engineer' }, { name: 'Aerospace Engineer' },
+		        { name: 'Agricultural Engineer' }, { name: 'Biological Engineer' },
+		        { name: 'Audio Engineer' }, { name: 'Bioengineering' },
+		        { name: 'Biochemical Engineer' }, { name: 'Biomedical Engineer' },
+		        { name: 'Ceramic Engineer' }, { name: 'Materials Engineer' },
+		        { name: 'Chemical Engineer' }, { name: 'Civil Engineer' },
+		        { name: 'Computer Engineer' }, { name: 'Environmental Engineer' },
+		        { name: 'Electrical Engineer' }, { name: 'Geological Engineer' }
+	        ],
+	        selectedOptions: [],
+	        query: ''
+        }
+	},
+	computed: {
+		filterOptions() {
+			if (this.query === '') {
+				return this.options
+			} else {
+				var ops = this.options
+				var filtered = []
+				ops.forEach(item => {
+					console.log(item);
+					if (item.name.startsWith(this.query)) {
+						filtered.push(item)
+						console.log(item)
+					}
+				});
+				return filtered
+			}
+		}
+	},
+	methods: {
+		find(value) {
+  	    	this.query = value
+		},
+	}
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
+
+/*.tooltip {*/
+  /*display: block !important;*/
+  /*z-index: 10000;*/
+/*}*/
+
+/*.tooltip .tooltip-inner {*/
+  /*background: black;*/
+  /*color: white;*/
+  /*border-radius: 16px;*/
+  /*padding: 5px 10px 4px;*/
+/*}*/
+
+/*.tooltip .tooltip-arrow {*/
+  /*width: 0;*/
+  /*height: 0;*/
+  /*border-style: solid;*/
+  /*position: absolute;*/
+  /*margin: 5px;*/
+  /*border-color: black;*/
+  /*z-index: 1;*/
+/*}*/
+
+/*.tooltip[x-placement^="top"] {*/
+  /*margin-bottom: 5px;*/
+/*}*/
+
+/*.tooltip[x-placement^="top"] .tooltip-arrow {*/
+  /*border-width: 5px 5px 0 5px;*/
+  /*border-left-color: transparent !important;*/
+  /*border-right-color: transparent !important;*/
+  /*border-bottom-color: transparent !important;*/
+  /*bottom: -5px;*/
+  /*left: calc(50% - 5px);*/
+  /*margin-top: 0;*/
+  /*margin-bottom: 0;*/
+/*}*/
+
+/*.tooltip[x-placement^="bottom"] {*/
+  /*margin-top: 5px;*/
+/*}*/
+
+/*.tooltip[x-placement^="bottom"] .tooltip-arrow {*/
+  /*border-width: 0 5px 5px 5px;*/
+  /*border-left-color: transparent !important;*/
+  /*border-right-color: transparent !important;*/
+  /*border-top-color: transparent !important;*/
+  /*top: -5px;*/
+  /*left: calc(50% - 5px);*/
+  /*margin-top: 0;*/
+  /*margin-bottom: 0;*/
+/*}*/
+
+/*.tooltip[x-placement^="right"] {*/
+  /*margin-left: 5px;*/
+/*}*/
+
+/*.tooltip[x-placement^="right"] .tooltip-arrow {*/
+  /*border-width: 5px 5px 5px 0;*/
+  /*border-left-color: transparent !important;*/
+  /*border-top-color: transparent !important;*/
+  /*border-bottom-color: transparent !important;*/
+  /*left: -5px;*/
+  /*top: calc(50% - 5px);*/
+  /*margin-left: 0;*/
+  /*margin-right: 0;*/
+/*}*/
+
+/*.tooltip[x-placement^="left"] {*/
+  /*margin-right: 5px;*/
+/*}*/
+
+/*.tooltip[x-placement^="left"] .tooltip-arrow {*/
+  /*border-width: 5px 0 5px 5px;*/
+  /*border-top-color: transparent !important;*/
+  /*border-right-color: transparent !important;*/
+  /*border-bottom-color: transparent !important;*/
+  /*right: -5px;*/
+  /*top: calc(50% - 5px);*/
+  /*margin-left: 0;*/
+  /*margin-right: 0;*/
+/*}*/
+
+/*.tooltip.popover .popover-inner {*/
+  /*background: #f9f9f9;*/
+  /*color: black;*/
+  /*padding: 24px;*/
+  /*border-radius: 5px;*/
+  /*box-shadow: 0 5px 30px rgba(black, .1);*/
+/*}*/
+
+/*.tooltip.popover .popover-arrow {*/
+  /*border-color: #f9f9f9;*/
+/*}*/
+
+/*.tooltip[aria-hidden='true'] {*/
+  /*visibility: hidden;*/
+  /*opacity: 0;*/
+  /*transition: opacity .15s, visibility .15s;*/
+/*}*/
+
+/*.tooltip[aria-hidden='false'] {*/
+  /*visibility: visible;*/
+  /*opacity: 1;*/
+  /*transition: opacity .15s;*/
+/*}*/
+
+
+
+/*.popover .popover-inner {*/
+  /*background: #f9f9f9;*/
+  /*color: black;*/
+  /*padding: 24px;*/
+  /*border-radius: 5px;*/
+  /*box-shadow: 0 5px 30px rgba(black, .1);*/
+/*}*/
+
+/*.popover .popover-arrow {*/
+  /*border-color: #f9f9f9;*/
+/*}*/
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VTooltip from 'v-tooltip'
+import Multiselect from 'vue-multiselect'
+
+// register globally
+Vue.component('multiselect', Multiselect)
+
+Vue.use(VTooltip);
+
 
 Vue.config.productionTip = false
 
